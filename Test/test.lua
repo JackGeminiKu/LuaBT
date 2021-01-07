b9ea8b45ea8cf6f49c53a6141308cdff96c94136
JackGeminiKu <JackGeminiKu@gmail.com>
2021-01-07 17:53:03 +0800

    1/7

--- Test/test.lua
+++ Test/test.lua
@@ -44,12 +44,12 @@ function Test:Run()
     -- bt5 = this:CreateBT5()
     -- bt5:EnabledBT()
 
-    local bt = this:CreateBT2()
+    local bt = this:CreateBT7()
     bt:EnabledBT()
     for i = 1, 100 do
         print('Update ' .. i)
         if bt:Update() ~= BT.ETaskStatus.Running then
-            break;
+            break
         end
     end
 end
@@ -133,131 +133,149 @@ function Test:CreateBT2()
 end
 
 function Test:CreateBT3()
-    local btree = BT.BTree:New(nil, nil)
+    local btree = BT.BTree:New(nil, 'BT3')
+
     -- 1
-    local sel1001 = BT.Selector:New()
+    local sel1001 = BT.Selector:New('sel1001')
     btree:AddRoot(sel1001)
+
     -- 2
-    local sel2001 = BT.Selector:New()
+    local sel2001 = BT.Selector:New('sel2001')
     sel2001:SetAbortType(BT.EAbortType.LowerPriority)
-    local seq2001 = BT.Sequence:New()
+    local seq2001 = BT.Sequence:New('seq2001')
     sel1001:AddChildList{sel2001, seq2001}
+
     -- 3
-    local seq3001 = BT.Sequence:New()
+    local seq3001 = BT.Sequence:New('seq3001')
     seq3001:SetAbortType(BT.EAbortType.LowerPriority)
-    local seq3002 = BT.Sequence:New()
+    local seq3002 = BT.Sequence:New('seq3002')
     seq3002:SetAbortType(BT.EAbortType.LowerPriority)
-    local rep3001 = BT.Repeater:New()
-    rep3001:SetExecutionCount(300)
+    local rep3001 = BT.Repeater:New('rep3001')
+    rep3001:SetExecutionCount(2)
     sel2001:AddChildList{seq3001, seq3002}
     seq2001:AddChildList{rep3001}
+
     -- 4
-    local bool4001 = BT.BoolComparison:New("bool1", true, false)
-    local rep4001 = BT.Repeater:New()
-    rep4001:SetExecutionCount(300)
+    local bool4001 = BT.BoolComparison:New("bool4001", true, false)
+    local rep4001 = BT.Repeater:New('rep4001')
+    rep4001:SetExecutionCount(2)
     seq3001:AddChildList{bool4001, rep4001}
-    local bool4002 = BT.BoolComparison:New("bool2", true, false)
-    local rep4002 = BT.Repeater:New()
-    rep4002:SetExecutionCount(300)
+    local bool4002 = BT.BoolComparison:New("bool4002", true, false)
+    local rep4002 = BT.Repeater:New('rep4002')
+    rep4002:SetExecutionCount(2)
     seq3002:AddChildList{bool4002, rep4002}
-    local seq4001 = BT.Sequence:New()
+    local seq4001 = BT.Sequence:New('seq4001')
     rep3001:AddChildList{seq4001}
 
     -- 5
-    local seq5001 = BT.Sequence:New()
+    local seq5001 = BT.Sequence:New('seq5001')
     rep4001:AddChildList{seq5001}
-    local seq5002 = BT.Sequence:New()
+    local seq5002 = BT.Sequence:New('seq5002')
     rep4002:AddChildList{seq5002}
-    local log5001 = BT.Log:New(nil, "this is Log5001")
-    local wait5001 = BT.Wait:New(nil, 0.1)
+    local log5001 = BT.Log:New('log5001', "this is Log5001")
+    local wait5001 = BT.Wait:New('wait5001', 0.1)
     seq4001:AddChildList{log5001, wait5001}
 
     -- 6
-    local log6001 = BT.Log:New(nil, "this is Log6001")
-    local wait6001 = BT.Wait:New(nil, 0.1)
+    local log6001 = BT.Log:New('log6001', "this is Log6001")
+    local wait6001 = BT.Wait:New('wait6001', 0.1)
     seq5001:AddChildList{log6001, wait6001}
-    local log6002 = BT.Log:New(nil, "this is Log6002")
-    local wait6002 = BT.Wait:New(nil, 0.1)
+    local log6002 = BT.Log:New('log6002', "this is Log6002")
+    local wait6002 = BT.Wait:New('wait6002', 0.1)
     seq5002:AddChildList{log6002, wait6002}
 
     return btree
 end
 
 function Test:CreateBT4()
-    local btree = BT.BTree:New(nil, nil)
+    local btree = BT.BTree:New(nil, 'BT4')
+
     -- 1
-    local sel1001 = BT.Selector:New()
+    local sel1001 = BT.Selector:New('sel1001')
     sel1001:SetAbortType(BT.EAbortType.Self)
     btree:AddRoot(sel1001)
+
     -- 2
-    local seq2001 = BT.Sequence:New()
+    local seq2001 = BT.Sequence:New('seq2001')
     seq2001:SetAbortType(BT.EAbortType.LowerPriority)
-    local rep2001 = BT.Repeater:New()
-    rep2001:SetRepeatForever(true)
+    local rep2001 = BT.Repeater:New('rep2001')
+    rep2001:SetExecutionCount(2)
+    -- rep2001:SetRepeatForever(true)
     sel1001:AddChildList{seq2001, rep2001}
+
     -- 3
-    local seq3001 = BT.Sequence:New()
+    local seq3001 = BT.Sequence:New('seq3001')
     seq3001:SetAbortType(BT.EAbortType.Both)
-    local log3001 = BT.Log:New(nil, "this is Log3001")
-    local log3002 = BT.Log:New(nil, "this is Log3002")
+    local log3001 = BT.Log:New('log3001', "this is Log3001")
+    local log3002 = BT.Log:New('log3002', "this is Log3002")
     seq2001:AddChildList{seq3001, log3001}
     rep2001:AddChildList{log3002}
-    --
-    local rep4001 = BT.Repeater:New()
-    local rep4002 = BT.Repeater:New()
-    rep4002:SetExecutionCount(100)
+
+    -- 4
+    local rep4001 = BT.Repeater:New('rep4001')
+    local rep4002 = BT.Repeater:New('rep4002')
+    rep4002:SetExecutionCount(2)
     seq3001:AddChildList{rep4001, rep4002}
+
     -- 5
-    local invt5001 = BT.Inverter:New()
+    local invt5001 = BT.Inverter:New('invt5001')
     rep4001:AddChildList{invt5001}
-    local seq5001 = BT.Sequence:New()
+    local seq5001 = BT.Sequence:New('seq5001')
     rep4002:AddChild(seq5001)
+
     -- 6
-    local rep6001 = BT.Repeater:New()
+    local rep6001 = BT.Repeater:New('rep6001')
     invt5001:AddChildList{rep6001}
-    local log6001 = BT.Log:New(nil, "this is Log6001")
-    local wait6001 = BT.Wait:New(nil, 0.5)
+    local log6001 = BT.Log:New('log6001', "this is Log6001")
+    local wait6001 = BT.Wait:New('wait6001', 0.5)
     seq5001:AddChildList{log6001, wait6001}
+
     -- 7
-    local invt7001 = BT.Inverter:New()
+    local invt7001 = BT.Inverter:New('invt7001')
     rep6001:AddChildList{invt7001}
+
     -- 8
-    local bool8001 = BT.BoolComparison:New("bool1", true, false)
+    local bool8001 = BT.BoolComparison:New("bool8004", true, false)
     invt7001:AddChildList{bool8001}
     return btree
 end
 
 function Test:CreateBT5()
-    local btree = BT.BTree:New(nil, nil)
+    local btree = BT.BTree:New(nil, 'BT5')
+
     -- 1
-    local sel1001 = BT.Selector:New()
+    local sel1001 = BT.Selector:New('sel1001')
     sel1001:SetAbortType(BT.EAbortType.Self)
     btree:AddRoot(sel1001)
+
     -- 2
-    local parallel2001 = BT.Parallel:New()
+    local parallel2001 = BT.Parallel:New('parallel2001')
     parallel2001:SetAbortType(BT.EAbortType.Self)
-    local rep2001 = BT.Repeater:New()
-    rep2001:SetExecutionCount(300)
+    local rep2001 = BT.Repeater:New('rep2001')
+    rep2001:SetExecutionCount(2)
     sel1001:AddChildList{parallel2001, rep2001}
+
     -- 3
-    local bool3001 = BT.BoolComparison:New("bool1", true, false)
-    local seq3001 = BT.Sequence:New()
+    local bool3001 = BT.BoolComparison:New("bool3001", true, false)
+    local seq3001 = BT.Sequence:New('seq3001')
     seq3001:SetAbortType(BT.EAbortType.Self)
-    local sel3001 = BT.Selector:New()
+    local sel3001 = BT.Selector:New('sel3001')
     sel3001:SetAbortType(BT.EAbortType.Self)
     parallel2001:AddChildList{bool3001, seq3001, sel3001}
-    local log3001 = BT.Log:New(nil, "this is Log3001")
+    local log3001 = BT.Log:New('log3001', "this is Log3001")
     rep2001:AddChildList{log3001}
+
     -- 4
-    local bool4001 = BT.BoolComparison:New("bool2", true, false)
-    local log4001 = BT.Log:New(nil, "this is Log4001")
+    local bool4001 = BT.BoolComparison:New("bool4001", true, false)
+    local log4001 = BT.Log:New('log4001', "this is Log4001")
     seq3001:AddChildList{bool4001, log4001}
-    local bool4002 = BT.BoolComparison:New("bool3", true, false)
-    local rep4001 = BT.Repeater:New()
+    local bool4002 = BT.BoolComparison:New("bool4002", true, false)
+    local rep4001 = BT.Repeater:New('rep4001')
     rep4001:SetRepeatForever(true)
     sel3001:AddChildList{bool4002, rep4001}
+
     -- 5
-    local log5001 = BT.Log:New(nil, "this is Log5001")
+    local log5001 = BT.Log:New('log5001', "this is Log5001")
     rep4001:AddChildList{log5001}
     return btree
 end
@@ -281,6 +299,18 @@ function Test:CreateBT6()
     rep2001:AddChild(ts13001)
     local ts23001 = BT.TestShared2:New("ts2-3001")
     rep2002:AddChild(ts23001)
+
+    return btree
+end
+
+function Test:CreateBT7()
+    local btree = BT.BTree:New(nil, "BT7")
+    local seq1001 = BT.Sequence:New("seq1001")
+    btree:AddRoot(seq1001)
+
+    local move2001 = BT.Move:New("move2001")
+    local buy2001 = BT.Buy:New("buy2001")
+    seq1001:AddChildList {move2001, buy2001}
 
     return btree
 end
